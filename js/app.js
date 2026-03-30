7d47bd3b088d78bb7e16c4b4b82af69811054d03
Joel Bidal <joelbidal1@hotmail.com>
2026-03-30 16:52:10 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,102 @@
+window.addEventListener('load', function() {  
+    // Fix navbar when scroll
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 1) {
+            $('nav').addClass('scrolled');
+        } 
+        else {
+            $('nav').removeClass('scrolled');
+        }
+    });
+
+    // Slick slider for Resources
+    if ($('.wrapper-phones').length) {
+        $('.wrapper-phones').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '0',
+            focusOnSelect: false,
+            arrows: false,
+            autoplay: false,
+            pauseOnHover: true,
+            draggable: true,
+            infinite: false,
+            dots: true,
+            mobileFirst: true,
+            responsive: [
+                {
+                    breakpoint: 991,
+                    settings: "unslick"
+                }
+            ]
+        });
+    }
+
+    // Animations on parallax
+    if (window.matchMedia('(min-width: 575px)').matches) {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.fromTo(".row-feature.is-left .inner-graphic img", {
+            y: -40
+            },
+            {
+            y: 60,
+            ease: "none",
+            scrollTrigger: {
+                trigger: ".row-feature.is-left",
+                start: "top bottom",
+                end: "bottom top",
+                scrub: true,
+            }
+        });
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.fromTo(".row-feature.is-right .inner-graphic img", {
+            y: -40
+            },
+            {
+            y: 60,
+            ease: "none",
+            scrollTrigger: {
+                trigger: ".row-feature.is-right",
+                start: "top bottom",
+                end: "bottom top",
+                scrub: true,
+            }
+        });
+    }
+
+    //   Animations on fade in
+    let animatedElements = new Set(); // Para evitar reanimaciones
+
+    let observer = new IntersectionObserver((entries) => {
+        // Filtrar los elementos que están entrando en vista y no han sido animados aún
+        const toAnimate = entries
+            .filter(entry => entry.isIntersecting && !animatedElements.has(entry.target))
+            .map(entry => entry.target);
+
+        if (toAnimate.length > 0) {
+            gsap.to(toAnimate, {
+                opacity: 1,
+                y: 0,
+                stagger: 0.15,
+                ease: "power2.out",
+                duration: 0.5,
+                delay: 0.3
+            });
+
+            // Marcar los elementos como animados y dejar de observarlos
+            toAnimate.forEach(el => {
+                animatedElements.add(el);
+                observer.unobserve(el);
+            });
+        }
+    }, {
+        threshold: 0.3
+    });
+
+    document.querySelectorAll("[data-fade]").forEach((el) => {
+        observer.observe(el);
+    });
+
+});
